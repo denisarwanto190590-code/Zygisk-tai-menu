bec8e5a2b09d5c10059f1674616b3386d9f5e6fe
denisarwanto190590-code <denisarwanto190590@gmail.com>
2026-07-17 18:53:53 +0700

    Update functions.h with hooks and data structures
    Refactor function declarations and add data structures for hooks.

--- module/src/main/cpp/functions.h
+++ module/src/main/cpp/functions.h
@@ -1,14 +1,33 @@
 #ifndef ZYCHEATS_SGUYS_FUNCTIONS_H
 #define ZYCHEATS_SGUYS_FUNCTIONS_H
 
-// here you can define variables for the patches
-bool addCurrency, freeItems, everythingUnlocked, showAllItems, addSkins;
+// --- LIBRARY UNTUK ESP/IMGUI ---
+#include <vector>
+#include <mutex>
 
+// --- STRUKTUR DATA (Wajib untuk koordinat) ---
+struct Vector3 {
+    float x, y, z;
+};
+
+// --- DECLARATION EKSTERNAL (Integrasi ke ImGui) ---
+// Variable & hook pointer yang akan di-eksekusi di dalam file .cpp ImGui
+extern void* main_camera;
+extern std::vector<void*> active_players;
+extern std::mutex player_mutex;
+extern Vector3 (*old_GetPosition)(void *instance);
+extern void *(*old_WorldToScreenPoint)(void *camera, Vector3 position, int eye, Vector3 *output);
+
+// --- VARIABLES UNTUK PATCHES ---
+bool addCurrency = false, freeItems = false, everythingUnlocked = false, showAllItems = false, addSkins = false;
+
+// --- UTILS ---
 monoString *CreateIl2cppString(const char *str) {
     monoString *(*String_CreateString)(void *instance, const char *str) = (monoString*(*)(void*, const char*)) (g_il2cppBaseMap.startAddress + string2Offset(OBFUSCATE("0x2596B20")));
     return String_CreateString(NULL, str);
 }
 
+// --- POINTERS & HOOKS ---
 void (*PurchaseRealMoney) (void* instance, monoString* itemId, monoString* receipt, void* callback);
 
 void Pointers() {
@@ -20,7 +39,7 @@ void Patches() {
     PATCH_SWITCH("0xF148A4", "E07C80D2C0035FD6", freeItems);
 }
 
-// declare your hooks here
+// --- FUNCTIONS HOOKING ---
 void (*old_Backend)(void *instance);
 void Backend(void *instance) {
     if (instance != NULL) {
@@ -29,10 +48,6 @@ void Backend(void *instance) {
             PurchaseRealMoney(instance, CreateIl2cppString("special_offer1"), CreateIl2cppString("dev"), NULL);
             addCurrency = false;
         }
-        if (addSkins) {
-            LOGW("Calling Skins");
-            addSkins = false;
-        }
     }
     return old_Backend(instance);
 }
@@ -40,10 +55,7 @@ void Backend(void *instance) {
 void* (*old_ProductDefinition)(void *instance, monoString* id, monoString* storeSpecificId, int type, bool enabled, void* payouts);
 void* ProductDefinition(void *instance, monoString* id, monoString* storeSpecificId, int type, bool enabled, void* payouts) {
     if (instance != NULL) {
-        LOGW("Called ProductDefinition! Here are the parameters:");
-        LOGW("id: %s", id->getChars());
-        LOGW("storeSpecificId: %s", storeSpecificId->getChars());
-        LOGW("type: %i", type);
+        LOGW("Product: %s | ID: %s", id->getChars(), storeSpecificId->getChars());
     }
     return old_ProductDefinition(instance, id, storeSpecificId, type, enabled, payouts);
 }
